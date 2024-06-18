ec82febc046af139604508b42d694aa023b44e58
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2024-06-18 17:41:30 +0530

    Time: 49 ms (64.69%) | Memory: 41.4 MB (35.05%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -2,14 +2,31 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int res=0;
+        vector<pair<int,int>>temp;
+        for(int i=0;i<profit.size();i++)
+        {
+            temp.push_back({difficulty[i],profit[i]});
+        }
+        sort(temp.begin(),temp.end());
+        vector<int>maxi(temp.size());
+        maxi[0]=temp[0].second;
+        for(int i=1;i<maxi.size();i++)maxi[i]=max(temp[i].second,maxi[i-1]);
         for(auto i:worker)
         {
-            int p=0;
-            for(int j=0;j<profit.size();j++)
+            int a=0,b=profit.size()-1,p=0;
+            while(a<=b)
             {
-                if(difficulty[j]<=i)p=max(p,profit[j]);
+                int m=(a+b)/2;
+                if(temp[m].first>i)
+                {
+                    b=m-1;
+                }
+                else
+                {
+                    p=max(p,maxi[m]);
+                    a=m+1;
+                }
             }
-            // cout<<p<<" ";
             res+=p;
         }
         return res;
