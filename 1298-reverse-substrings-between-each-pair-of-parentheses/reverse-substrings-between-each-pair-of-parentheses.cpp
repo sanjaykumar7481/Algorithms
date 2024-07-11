77dc492e07353b7b2fbe42d23a4f25720782759b
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2024-07-11 17:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (30.07%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -3,28 +3,41 @@ public:
     string reverseParentheses(string s) {
         stack<string>st;
         string temp="",ans;
+        int par=0;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(')
             {
-                if(temp!="")st.push(temp);
+                if(temp!="")
+                {
+                    if(par%2)reverse(temp.begin(),temp.end());
+                    st.push(temp);
+                }
                 st.push("(");
                 temp="";
+                par++;
             }
             else if(s[i]==')')
             {
                 string res;
-                if(temp!="")st.push(temp);
+                if(temp!="") {
+                    if(par%2)reverse(temp.begin(),temp.end());
+                    st.push(temp);
+                }
                 while(st.top()!="(")
                 {
                     string t=st.top();
                     st.pop();
-                    reverse(t.begin(),t.end());
-                    res+=t;
+                    // reverse(t.begin(),t.end());
+                    if(par%2==0)res=t+res;
+                    else res+=t;
                 }
                 st.pop();
+                // if(par%2==0)
+                // reverse(res.begin(),res.end());
                 st.push(res);
                 temp="";
+                par--;
             }
             else temp+=s[i];
         }
