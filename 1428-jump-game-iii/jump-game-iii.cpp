9f1859d7eaa00c69fefbf443752cf8b6fc1181de
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2024-07-25 15:40:14 +0530

    Time: 144 ms (26.29%) | Memory: 51.1 MB (9.82%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool dfs(int i,vector<int>&arr,vector<int>&vis,vector<bool>&dp)
+    {
+        if(i<0 or i>=arr.size() or vis[i])return false;
+        if(dp[i]==true)return true;
+        vis[i]=1;
+        dp[i]=dfs(i+arr[i],arr,vis,dp) or dfs(i-arr[i],arr,vis,dp);
+        return dp[i];
+    }
+    bool canReach(vector<int>& arr, int start) {
+        vector<bool>dp(arr.size(),false);
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]==0)dp[i]=true;
+        }
+        vector<int>vis(arr.size(),0);
+        bool d=dfs(start,arr,vis,dp);
+        // if(dp[start])return true;
+        return dp[start];
+    }
+};
