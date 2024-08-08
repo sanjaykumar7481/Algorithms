144b64bb116b9bd166618c7ef8c1923ba67fdc7c
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2024-08-08 14:29:39 +0530

    Time: 15 ms (11.84%) | Memory: 14 MB (12.09%) - LeetSync

---
+++ 921-spiral-matrix-iii/spiral-matrix-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>>res;
+        int count=rows*cols;
+        count--;
+        vector<vector<int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
+        res.push_back({rStart,cStart});
+        int p=0,size=1;
+        while(count)
+        {
+            for(int i=0;i<size;i++)
+            {
+                rStart+=dir[p][0];
+                cStart+=dir[p][1];
+                if(rStart>=0 and rStart<rows and cStart>=0 and cStart<cols)
+                {
+                    // cout<<rStart<<" "<<cStart<<endl;
+                    count--;
+                    res.push_back({rStart,cStart});
+                }
+            }
+            p=(p+1)%4;
+            if(p%2==0)size++;
+        }
+        return res;
+    }
+};
