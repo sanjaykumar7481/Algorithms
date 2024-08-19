056553b934bd13dec2c23ebb277d1ca1181ad756
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2024-08-19 22:46:13 +0530

    Time: 15 ms (36.48%) | Memory: 157.1 MB (9.34%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int fun(int i,int size,int n){
+    int fun(int i,int size,int n,vector<vector<int>>&dp){
         if(i==n)return 0;
+        if(dp[i][size]!=-1)return dp[i][size];
         int ans=1e9;
-        if(i+i<=n)ans=min(ans,2+fun(i+i,i,n));
-        if(size!=0 and i+size<=n)ans=min(ans,1+fun(i+size,size,n));
+        if(i+i<=n)ans=min(ans,2+fun(i+i,i,n,dp));
+        if(size!=0 and i+size<=n)ans=min(ans,1+fun(i+size,size,n,dp));
         if(ans>1e9)ans=1e9;
-        return ans;
+        return dp[i][size]=ans;
     }
     int minSteps(int n) {
-        return fun(1,0,n);
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return fun(1,0,n,dp);
     }
 };
