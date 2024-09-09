f9ba54a9be7a33137a026ccc24577591b8292d53
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2024-09-09 14:53:37 +0530

    Time: 176 ms (22.52%) | Memory: 130.5 MB (65.45%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -18,19 +18,17 @@ public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
         vector<vector<int>>res(m,vector<int>(n,-1));
         vector<vector<int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
-        int p=0;
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        while(!q.empty() and head)
+        int p=0,x=0,y=0;
+        while(head)
         {
-            auto a=q.front();
-            int x=a.first,y=a.second;
-            q.pop();
             res[x][y]=head->val;
             head=head->next;
             if(!valid(x+dir[p][0], y+dir[p][1],res))  p=(p+1)%4;
             if(valid(x+dir[p][0], y+dir[p][1],res))
-                q.push({x+dir[p][0],y+dir[p][1]});
+            {
+                x+=dir[p][0];
+                y+=dir[p][1];
+            }
             
         }
         return res;
