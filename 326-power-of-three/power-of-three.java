84eb4d1a792a6dd0377adf9c951c16c1f02546ad
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2025-08-13 20:18:52 +0530

    Time: 8 ms (84.41%) | Memory: 44.8 MB (5.71%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
         double k=Math.log10(n)/(double)Math.log10(3);
-        System.out.println(k);
+        // System.out.println(k);
         return k==(int)k;
 
     }
